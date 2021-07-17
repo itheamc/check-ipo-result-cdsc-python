42500f6d3022cff6899b2da85d99f4285bc23547
Amit Chaudhary <itheamc@gmail.com>
2021-07-17 09:33:53 +0545

    Third Commit - Code Optimization

--- main.py
+++ main.py
@@ -2,23 +2,22 @@
 import ast
 from ipo_result import IpoResult
 
-# Getting the list of companies from the server and proceeds the response
-response = IpoResult.fetch_companies()
-response = response.replace('true', 'True')
-response = ast.literal_eval(response)
-companies = response['body']
 
-# Printing the Header
-print(
-    '------------------------------------------------------------------------------------\n\t\t------------------------ CHECK IPO RESULTS -----------------------\n------------------------------------------------------------------------------------')
-# Printing the companies name as a option for the user
-for company in companies:
-    print(f"[✓] Enter __ {company['id']} __ for {company['name']}")
+# Creating function to process the company list
+def process_company_lists(response):
+    _res = response
+    if 'true' in _res:
+        _res = _res.replace("true", "True")
+    if 'false' in _res:
+        _res = _res.replace("false", "False")
+
+    _res = ast.literal_eval(_res)
+    return _res['body']
 
 
 # Creating function to process the response and convert on dictionary type
-def process_response(res):
-    _res = res
+def process_result_response(response):
+    _res = response
     if 'true' in _res:
         _res = _res.replace("true", "True")
     if 'false' in _res:
@@ -32,6 +31,21 @@ def process_response(res):
     return _res
 
 
+# -----------------------------------------------------------
+# Below codes are for printing information on the console
+# -----------------------------------------------------------
+
+# Printing the Header
+print(
+    '------------------------------------------------------------------------------------\n\t\t------------------------ CHECK IPO RESULTS -----------------------\n------------------------------------------------------------------------------------')
+# Getting the list of companies from the server and proceeds the response
+companies = IpoResult.fetch_companies()
+# Processing the response
+proceed_companies_dict = process_company_lists(companies)
+# Printing the companies name as a option for the user
+for company in proceed_companies_dict:
+    print(f"[✓] Enter __ {company['id']} __ for {company['name']}")
+
 while True:
     # Getting input from the user
     comp_id = input('\n[+] Enter Here_    ')
@@ -39,6 +53,6 @@ while True:
 
     # Checking whether share is allotted or not and processing the response to convert in dict
     res = IpoResult.check_ipo_results(comp_id, user_boid)
-    _proceed_res = process_response(res)
+    _proceed_res = process_result_response(res)
 
     print(f"\n\t\t_______ {_proceed_res['message']} _______")
