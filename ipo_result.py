803c51c34e3d0167c965f6e9421b992b59b719d8
Amit Chaudhary <itheamc@gmail.com>
2021-07-17 09:49:33 +0545

    Forth Commit - Addition of Function to proceed response

--- ipo_result.py
+++ ipo_result.py
@@ -1,3 +1,4 @@
+import ast
 import json
 import requests as requests
 from urls import FETCH_COMPANY_URL, IPO_CHECK_URL
@@ -8,14 +9,14 @@ class IpoResult:
     def __init__(self):
         pass
 
-    # Static method to fetch the companies
+    # Function to fetch the companies
     @staticmethod
-    def fetch_companies():
+    def __fetch_companies():
         return requests.request('get', url=FETCH_COMPANY_URL).text
 
     # Function to check the ipo results
     @staticmethod
-    def check_ipo_results(company_id, boid):
+    def __check_ipo_results(company_id, boid):
         res = None
         try:
             headers = {'Content-Type': 'application/json', 'Accept': 'application/json, text/plain, */*'}
@@ -25,3 +26,32 @@ class IpoResult:
             res = {'message': e.msg}
 
         return res
+
+    # Creating function to process the company list
+    @staticmethod
+    def get_company_list():
+        _res = IpoResult.__fetch_companies()
+        if 'true' in _res:
+            _res = _res.replace("true", "True")
+        if 'false' in _res:
+            _res = _res.replace("false", "False")
+
+        _res = ast.literal_eval(_res)
+        return _res['body']
+
+    # Creating function to process the response and convert on dictionary type
+    @staticmethod
+    def get_ipo_result(company_id, boid):
+        _res = IpoResult.__check_ipo_results(company_id=company_id, boid=boid)
+        if 'true' in _res:
+            _res = _res.replace("true", "True")
+        if 'false' in _res:
+            _res = _res.replace("false", "False")
+        if 'null' in _res:
+            _res = _res.replace("null", "None")
+        try:
+            _res = ast.literal_eval(_res)
+        except SyntaxError as se:
+            _res = {'message': se.msg}
+        return _res
+
